625123aad7b16a8d005b5b9c823dbb15ab31968c
aja63 <44000188+aja63@users.noreply.github.com>
2019-03-20 16:57:08 -0400

    Add files via upload
    resource gathering and count

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -5,7 +5,8 @@ var isUnitSelected = false;
 var altControl = false;
 var unitCount = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var unitCounter = 4;
-
+var resourceCount = 50;
+txt.innerHTML = resourceCount;
 
 document.addEventListener("keypress", pressed)
 document.addEventListener("keyup", unpressed)
@@ -81,17 +82,32 @@ function deselectUnit(){if (altControl == true){
 
 document.getElementById('creator').addEventListener("click", buildUnit);
 
-function buildUnit() {
+function buildUnit() { if (resourceCount >= 100){
+	resourceCount = resourceCount - 100;
 	var newUnit = document.createElement("div");
 	newUnit.classList.add('ally');
 	newUnit.style.top = "50px";
 	newUnit.style.left = "50px";
-	txt.innerHTML += unitCount;
 	newUnit.id = "playerUnit" + unitCount[unitCounter];
 	unitCounter = unitCounter + 1;
 	document.body.appendChild(newUnit);
 }
-	
-	
-	
+}
 
+document.addEventListener("click", getResource);
+
+function getResource(){
+	var resource = document.getElementById('resourceHolder');
+	var unitList = document.getElementsByClassName('selected');
+	for (unitCheck = 0; unitCheck < unitList.length; unitCheck++){
+		var unitID = unitList[unitCheck].id;
+		var unit = document.getElementById(unitID);
+		if (parseInt(unit.style.left) > parseInt(resource.style.left) && parseInt(unit.style.left)+50 < parseInt(resource.style.left)+250 && parseInt(unit.style.top) > parseInt(resource.style.top) && parseInt(unit.style.top)+50 < parseInt(resource.style.top)+250){ 
+			unit.classList.add('gathering');}
+			else{unit.classList.remove('gathering');}
+}
+}
+
+function resourceGathering(){
+	setInterval(function(){var unitsGathering = document.getElementsByClassName('gathering').length; resourceCount = resourceCount + unitsGathering*50; txt.innerHTML += resourceCount; txt.innerHTML += unitsGathering;}, 5000);
+}
